e6d6a4ca993cadfef81d9ef66be79a84269c2479
Aniket Patil <114171294+aniketpatil09@users.noreply.github.com>
2024-02-05 15:06:23 +0530

    Add files via upload

---
+++ dentist website/index.php
@@ -0,0 +1,343 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','contact_form') or die('connection failed');
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $number = $_POST['number'];
+   $date = $_POST['date'];
+
+   $insert = mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, date) VALUES('$name','$email','$number','$date')") or die('query failed');
+
+   if($insert){
+      $message[] = 'appointment made successfully!';
+   }else{
+      $message[] = 'appointment failed';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Complete Responsive Dentist Website Design Tutorial</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- bootstrap cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<header class="header fixed-top">
+
+   <div class="container">
+
+      <div class="row align-items-center justify-content-between">
+
+         <a href="#home" class="logo">दंत-क्रांति<span> क्लिनिक</span></a>
+
+         <nav class="nav">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#services">services</a>
+            <a href="#reviews">reviews</a>
+            <a href="#contact">contact</a>
+         </nav>
+
+         <a href="#contact" class="link-btn">make appointment</a>
+
+         <div id="menu-btn" class="fas fa-bars"></div>
+
+      </div>
+
+   </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+   <div class="container">
+
+      <div class="row min-vh-100 align-items-center">
+         <div class="content text-center text-md-left">
+            <h3>let us brighten your smile</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium itaque, quasi aliquam alias tempora voluptatibus.</p>
+            <a href="#contact" class="link-btn">make appointment</a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+   <div class="container">
+
+      <div class="row align-items-center">
+
+         <div class="col-md-6 image">
+            <img src="images/about-img.jpg" class="w-100 mb-5 mb-md-0" alt="">
+         </div>
+
+         <div class="col-md-6 content">
+            <span>about us</span>
+            <h3>True Healthcare For Your Family</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam cupiditate vero in provident ducimus. Totam quas labore mollitia cum nisi, sint, expedita rem error ipsa, nesciunt ab provident. Aperiam, officiis!</p>
+            <a href="#contact" class="link-btn">make appointment</a>
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+   <h1 class="heading">our services</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/icon-1.svg" alt="">
+         <h3>Alignment specialist</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, non?</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-2.svg" alt="">
+         <h3>Cosmetic dentistry</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, non?</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-3.svg" alt="">
+         <h3>Oral hygiene experts</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, minima?</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-4.svg" alt="">
+         <h3>Root canal specialist</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, minima?</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-5.svg" alt="">
+         <h3>Live dental advisory</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, minima?</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-6.svg" alt="">
+         <h3>Cavity inspection</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, minima?</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- process section starts  -->
+
+<section class="process">
+
+   <h1 class="heading">work process</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/process-1.png" alt="">
+         <h3>Cosmetic Dentistry</h3>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, excepturi?</p>
+      </div>
+
+      <div class="box">
+         <img src="images/process-2.png" alt="">
+         <h3>Pediatric Dentistry</h3>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, excepturi?</p>
+      </div>
+
+      <div class="box">
+         <img src="images/process-3.png" alt="">
+         <h3>Dental Implants</h3>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, excepturi?</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- process section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews" id="reviews">
+
+   <h1 class="heading"> satisfied clients </h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+      <img src="images\ak.jpeg" alt="">
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, iure? Nemo est aspernatur voluptatum id, laboriosam asperiores iure omnis alias?</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Akash Sawant</h3>
+         <span>satisfied client</span>
+      </div>
+
+      <div class="box">
+      <img src="images\sk.jpeg" alt="">
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, iure? Nemo est aspernatur voluptatum id, laboriosam asperiores iure omnis alias?</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Sanket Kachare</h3>
+         <span>satisfied client</span>
+      </div>
+
+      <div class="box">
+      <img src="images\sb.jpeg" alt="">
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, iure? Nemo est aspernatur voluptatum id, laboriosam asperiores iure omnis alias?</p>
+         <div class="stars">
+            <i  class="fas fa-star" ></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Shubham Borage</h3>
+         <span>satisfied client</span>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+   <h1 class="heading">make appointment</h1>
+
+   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <?php 
+         if(isset($message)){
+            foreach($message as $message){
+               echo '<p class="message">'.$message.'</p>';
+            }
+         }
+      ?>
+      <span>your name :</span>
+      <input type="text" name="name" placeholder="enter your name" class="box" required>
+      <span>your email :</span>
+      <input type="email" name="email" placeholder="enter your email" class="box" required>
+      <span>your number :</span>
+      <input type="number" name="number" placeholder="enter your number" class="box" required>
+      <span>appointment date :</span>
+      <input type="datetime-local" name="date" class="box" required>
+      <input type="submit" value="make appointment" name="submit" class="link-btn">
+   </form>  
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container container">
+
+      <div class="box">
+         <i class="fas fa-phone"></i>
+         <h3>phone number</h3>
+         <p>+91 8605038900</p>
+         <p>+91 7792870909</p>
+      </div>
+      
+      <div class="box">
+         <i class="fas fa-map-marker-alt"></i>
+         <h3>Our Address</h3>
+         <p>Karad,Satara,415539</p>
+         <iframe src=
+         "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4531.258563896231!2d74.17625197153859!3d17.251791099146647!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc182e395ef2671%3A0x719d14ba23859f1b!2sBharati%20Vidyapeeth%20Deemed%20University%2C%20Yashwantrao%20Mohite%20Institute%20of%20Management%2C%20Malkapur%2C%20Karad!5e0!3m2!1sen!2sin!4v1652896911948!5m2!1sen!2sin">
+            </iframe>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-clock"></i>
+         <h3>opening hours</h3>
+         <p>10.00am to 05:00pm</p>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-envelope"></i>
+         <h3>email address</h3>
+         <p>asp.karad7818@gmail.com</p>
+      </div>
+
+   </div>
+
+   <div class="credit"> &copy; copyright @ <?php echo date('Y'); ?>-  by <span>Mr. Aniket Patil , Mr.Aftab Ambekari , Mr.Dhiraj Gholap</span>  </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
